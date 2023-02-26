a3185fbb0bb09ba34e772f2ba8535dbd16d0e931
Simon Hall <34691264+hallsptcd@users.noreply.github.com>
2023-02-26 18:08:46 +0000

    Add files via upload

---
+++ Reducer.java
@@ -0,0 +1,21 @@
+package MapReduceWC;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+public void reduce(Text key, Iterable<IntWritable> values, Context context)
+throws IOException, InterruptedException {
+int wordCount = 0;
+System.out.println("In Reducer now!");
+for (IntWritable value : values) {
+wordCount += value.get();
+}
+context.write(key, new IntWritable(wordCount));
+}
+
+}
